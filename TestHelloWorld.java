2a269b48dd1a053795025b16d597b00b131d019d
AnTRaX69 <bebexinhox@hotmail.com>
2014-06-27 11:43:19 +0100

    Update TestHelloWorld.java

--- TestHelloWorld.java
+++ TestHelloWorld.java
@@ -2,9 +2,9 @@ public class TestHelloWorld {
 	
 	public static void main(String[] args) {
 		
-		System.out.println("Hello World!!!");
+		System.out.println("Hello MEEE!!!");
 		
-		System.out.println("Hello Universe!!!");
+		System.out.println("Hello MME TOOOO!!!");
 		
 		// above code is added by ARC-tester
 		String str = "*";
